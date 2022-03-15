382df25ce45cf910cb6bd02a10dcd29e1ea9f7b3
qod3r <primculov@protonmail.ch>
2022-03-15 21:08:50 +0700

    more fixes

--- func/alice_friends.py
+++ func/alice_friends.py
@@ -6,7 +6,7 @@ def add_friends(name, friends):
 
 
 def are_friends(name1, name2):
-    return any(name2 in alices_friends[name1], name1 in alices_friends[name2])
+    return name2 in alices_friends[name1]
 
 
 def print_friends(name):
@@ -14,3 +14,8 @@ def print_friends(name):
 
 
 alices_friends = dict()
+
+add_friends("Алла", ["Марина", "Иван"])
+print(are_friends("Алла", "Мария"))
+add_friends("Алла", ["Мария"])
+print(are_friends("Алла", "Мария"))
